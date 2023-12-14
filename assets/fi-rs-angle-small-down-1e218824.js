8b92c420c753d087cbf4a788d6c2fe7fab48f14f
Kang Eangchheang <128678739+KangEangchheang@users.noreply.github.com>
2023-12-14 19:20:20 +0700

    Delete assets directory

--- assets/fi-rs-angle-small-down-1e218824.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/fi-rs-angle-small-up-9bd4b518.svg",a="/assets/fi-rs-angle-small-down-ac31556d.svg";export{s as _,a};
--- assets/fi-rs-angle-small-right-2-b3a89b6f.js
+++
@@ -1,1 +0,0 @@
-const s="/assets/fi-rs-angle-small-right-2-e287bf50.svg";export{s as _};
